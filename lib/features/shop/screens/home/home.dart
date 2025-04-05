d3aa4e051cc52070be9d50de5eb06d7abf104747
Sahil kumar <stxnsick@gmail.com>
2025-04-05 20:10:08 +0530

    new files added on 4-5-23.6:32

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
-import 'package:t_store/common/widgets/appbar/appbar.dart';
 import 'package:t_store/features/shop/screens/home/widgets/home_appbar.dart';
 
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
-import '../../../../common/widgets/products/cart/cart_menu_icon.dart';
-import '../../../../utils/constants/colors.dart';
-import '../../../../utils/constants/text_strings.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
