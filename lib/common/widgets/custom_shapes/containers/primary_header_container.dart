251e6556764b2187f2649de7ff7f310696e9c8cb
Sahil kumar <stxnsick@gmail.com>
2025-04-05 20:08:41 +0530

    new files added on 4-5-23.6:32

--- lib/common/widgets/custom_shapes/containers/primary_header_container.dart
+++ lib/common/widgets/custom_shapes/containers/primary_header_container.dart
@@ -6,25 +6,33 @@ import 'circular_container.dart';
 
 class TPrimaryHeaderContainer extends StatelessWidget {
   const TPrimaryHeaderContainer({
-    super.key, required this.child,
+    super.key,
+    required this.child,
   });
+
   final Widget child;
+
   @override
   Widget build(BuildContext context) {
     return TCurvedEdgesWidgets(
       child: Container(
         color: TColors.primary,
         padding: const EdgeInsets.all(0),
+        child: Stack(
+          children: [
+            // Background circular decorations
+            Positioned(top: -150, right: -250,
+                child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1))),
+            Positioned(top: 100, right: -300,
+                child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1))),
 
-        child:  SizedBox(
-          width: double.infinity,
-          height: 400,
-          child: Stack(
-            children: [
-              Positioned(top:-150,right: -250, child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1))),
-              Positioned(top:100,right: -300, child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1))),
-            ],
-          ),
+            // Actual content (the child widget)
+            SizedBox(
+              width: double.infinity,
+              height: 400,
+              child: child, // This is where your content goes
+            ),
+          ],
         ),
       ),
     );
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:t_store/common/widgets/appbar/appbar.dart';
+import 'package:t_store/features/shop/screens/home/widgets/home_appbar.dart';
 
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
+import '../../../../common/widgets/products/cart/cart_menu_icon.dart';
+import '../../../../utils/constants/colors.dart';
+import '../../../../utils/constants/text_strings.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
+    return const Scaffold(
+
       body: SingleChildScrollView(
         child: Column(
           children: [
             TPrimaryHeaderContainer(
-            child:Container()
+            child:Column(
+              children:[
+                THomeAppBar()
+              ],
+            )
             ),
               ],
             ),
@@ -21,6 +31,9 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+
+
 
 
 
